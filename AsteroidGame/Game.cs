d9d63e4a8fdef4fa05c22e81576e14f58a7664d5
AvramovDenis <avramov.91@mail.ru>
2020-04-22 13:52:28 +0300

    Начальная настройка классов Game и Programm

--- AsteroidGame/Game.cs
+++ AsteroidGame/Game.cs
@@ -4,19 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace AsteroidGame
 {
     static class Game
     {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
 
-        public static void Init(Form form)
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game()
         {
 
         }
+
+        public static void Init(Form form)
+        {
+            Graphics graphics;
+            graphics = form.CreateGraphics();
+
+            _context = BufferedGraphicsManager.Current;
+            
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+
+            Buffer = _context.Allocate(graphics, new Rectangle(0, 0, Width, Height));
+        }
+
         public static void Draw()
         {
-
+            Buffer.Graphics.Clear(Color.Black);
+            Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            Buffer.Render();
         }
 
     }
