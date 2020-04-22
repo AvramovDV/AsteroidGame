9eab25ef78d49746f15f0cae5dc7a8d3372b10a2
AvramovDenis <avramov.91@mail.ru>
2020-04-22 14:14:04 +0300

    Доработал класс Game для работы с BaseObject

--- AsteroidGame/Game.cs
+++ AsteroidGame/Game.cs
@@ -13,6 +13,8 @@ namespace AsteroidGame
         private static BufferedGraphicsContext _context;
         public static BufferedGraphics Buffer;
 
+        public static BaseObject[] Objects;
+
         public static int Width { get; set; }
         public static int Height { get; set; }
 
@@ -32,14 +34,49 @@ namespace AsteroidGame
             Height = form.ClientSize.Height;
 
             Buffer = _context.Allocate(graphics, new Rectangle(0, 0, Width, Height));
+            Load();
+
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        public static void Load()
+        {
+            Objects = new BaseObject[30];
+            for (int i = 0; i < Objects.Length; i++)
+            {
+                Objects[i] = new BaseObject(new Point(600, i * 20), new Point(15 - i, 15 - i), new Size(20, 20));
+            }
         }
 
         public static void Draw()
         {
+            //Buffer.Graphics.Clear(Color.Black);
+            //Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            //Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            //Buffer.Render();
+
             Buffer.Graphics.Clear(Color.Black);
-            Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
-            Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            for (int i = 0; i < Objects.Length; i++)
+            {
+                Objects[i].Draw();
+            }
             Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            for (int i = 0; i < Objects.Length; i++)
+            {
+                Objects[i].Update();
+            }
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
         }
 
     }
