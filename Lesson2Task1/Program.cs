501e45fe424eb6b3046dcc0fb2ecaf43d8724ca3
AvramovDenis <avramov.91@mail.ru>
2020-04-27 15:00:20 +0300

    Задал массив работников с разной формой оплаты

--- Lesson2Task1/Program.cs
+++ Lesson2Task1/Program.cs
@@ -8,8 +8,22 @@ namespace Lesson2Task1
 {
     class Program
     {
+        private static AbstractWorker[] _workers = new AbstractWorker[4]
+        {
+            new WorkerFixSalary("Фред", 35000f),
+            new WorkerHourSalary("Джон", 200f),
+            new WorkerHourSalary("Стив", 300f),
+            new WorkerFixSalary("Макс", 50000f)
+        };
+
         static void Main(string[] args)
         {
+            for (int i = 0; i < _workers.Length; i++)
+            {
+                Console.WriteLine($"{_workers[i].Name} получает в месяц: {_workers[i].GetSalary()}");
+            }
+            
+            Console.ReadKey();
         }
     }
 }
