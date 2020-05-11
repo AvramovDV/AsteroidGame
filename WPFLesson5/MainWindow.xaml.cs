4f10096214a4f29336c2f040fcd8e064358b8d30
AvramovDenis <avramov.91@mail.ru>
2020-05-11 14:34:17 +0300

    Формирую списки отделов и сотрудников

--- WPFLesson5/MainWindow.xaml.cs
+++ WPFLesson5/MainWindow.xaml.cs
@@ -20,9 +20,32 @@ namespace WPFLesson5
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Department> _departments;
+        private List<Employee> _employees;
+
         public MainWindow()
         {
             InitializeComponent();
+            MyListInI();
         }
+
+        private void MyListInI()
+        {
+            _departments = new List<Department>(5);
+            _departments.Add(new Department("Отдел 1"));
+            _departments.Add(new Department("Отдел 2"));
+            _departments.Add(new Department("Отдел 3"));
+            _departments.Add(new Department("Отдел 4"));
+            _departments.Add(new Department("Отдел 5"));
+
+            _employees = new List<Employee>(5);
+            _employees.Add(new Employee("Петров", _departments[0]));
+            _employees.Add(new Employee("Иванов", _departments[1]));
+            _employees.Add(new Employee("Сидоров", _departments[2]));
+            _employees.Add(new Employee("Слащев", _departments[3]));
+            _employees.Add(new Employee("Игнатьев", _departments[4]));
+            
+        }
+
     }
 }
