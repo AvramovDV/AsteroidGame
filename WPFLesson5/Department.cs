3afb0b4334a69722e918e2020db6b74688077d1d
AvramovDenis <avramov.91@mail.ru>
2020-05-11 14:19:00 +0300

    Добавил классы Сотрудник (Employee) и Отдел (Department)

---
+++ WPFLesson5/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLesson5
+{
+    internal class Department
+    {
+        private string _name;
+
+        public string Name { get => _name; }
+
+        public Department(string name)
+        {
+            _name = name;
+        }
+    }
+}
