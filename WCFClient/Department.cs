da10c4a4f3a1fe67c9456b8c77c756d2d3cb099e
AvramovDenis <avramov.91@mail.ru>
2020-05-20 20:10:13 +0300

    Добавил классы Department и Employee

---
+++ WCFClient/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFClient
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public List<Employee> Employes { get; set; } = new List<Employee>();
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
